c5abd97179d090cec2f54e7ed1c12ef9e5aa5456
satoshi kato <nowallcloud9+kato@gmail.com>
2016-12-24 06:33:12 +0000

    詳細機能作成よくわからん

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,15 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+  def show
+    @book =  Book.find(params[:id])
+    #例 books/15というURLのときは、params[:id]は15になる
+  end
+  
+  def new
+    @book = Book.new
+  end
+  def create 
   end
 end
