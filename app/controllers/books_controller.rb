2f4a8c6498ff513d642257381def37cd5a163dd5
satoshi kato <nowallcloud9+kato@gmail.com>
2016-12-24 08:10:36 +0000

    削除機能仕組みがよくわからん

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+  
   def show
     @book =  Book.find(params[:id])
     #例 books/15というURLのときは、params[:id]は15になる
@@ -10,17 +11,36 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+  
   def create 
     @book = Book.new(book_params)
     @book.save
     redirect_to books_path
   end
-# ここからはprivate #
 
-private
-def book_params
-    params.require(:book).permit(:name, :price, :publish, :publish_date)
+  def edit
+    @book = Book.find(params[:id])
+
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
+  end
   
-end
+  def destroy
+    # URLで指定した番号のモデルを削除する
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+# ここからはprivate #
+  private
+    def book_params
+      params.require(:book).permit(:name, :price, :publish, :publish_date)
+
+    end
 
 end
